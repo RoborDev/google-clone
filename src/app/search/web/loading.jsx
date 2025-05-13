0a41e7638dffc33241100c054b1bc3604d0862a8
RoborDev <esioleerobor@gmail.com>
2025-05-13 16:25:04 +0100

    Add loading effect for web,image,video pages

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="">
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="">
+          <div className=" bg-gray-200 rounded-full mb-2.5 h-2.5 w-48"></div>
+          <div className=" bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px]"></div>
+          <div className=" h-2 bg-gray-200 rounded-full mb-2.5 max-w-[560px]"></div>
+          <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[530px]"></div>
+        </div>
+      </div>
+
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="">
+          <div className=" bg-gray-200 rounded-full mb-2.5 h-2.5 w-48"></div>
+          <div className=" bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px]"></div>
+          <div className=" h-2 bg-gray-200 rounded-full mb-2.5 max-w-[560px]"></div>
+          <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[530px]"></div>
+        </div>
+      </div>
+
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className="">
+          <div className=" bg-gray-200 rounded-full mb-2.5 h-2.5 w-48"></div>
+          <div className=" bg-gray-200 rounded-full mb-2.5 h-3.5 max-w-[360px]"></div>
+          <div className=" h-2 bg-gray-200 rounded-full mb-2.5 max-w-[560px]"></div>
+          <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[530px]"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
